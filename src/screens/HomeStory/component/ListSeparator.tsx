dbe0f414686b7eabb1cb20dca8a6e29104b4f914
Mahmoud Aziz <mahmoudabdelaziz565@gmail.com>
2022-09-14 21:11:55 +0200

    Create list separator component

---
+++ src/screens/HomeStory/component/ListSeparator.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, View} from 'react-native';
+import React from 'react';
+
+const ListSeparator = () => {
+  return <View style={styles.container} />;
+};
+
+export default ListSeparator;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 8,
+  },
+});
