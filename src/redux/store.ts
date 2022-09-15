74749c37baf5f0e651c460c61c4e2bb28090fe5d
Mahmoud Aziz <mahmoudabdelaziz565@gmail.com>
2022-09-15 13:44:24 +0200

    Persist cart data

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,22 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import CartReducer from './cartSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistReducer, persistStore} from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
+  cart: CartReducer,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    cart: CartReducer,
-  },
+  reducer: persistedReducer,
 });
+const persistor = persistStore(store);
 
-export default store;
+export {store, persistor};
